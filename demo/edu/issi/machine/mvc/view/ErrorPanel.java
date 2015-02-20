9c9fe27a20cd22887ea6443a8f98fa5afdc631dd
dawidsamolyk <dawid.samolyk@gmail.com>
2015-02-20 18:16:04 +0100

    Początek nowego GUI.

---
+++ demo/edu/issi/machine/mvc/view/ErrorPanel.java
@@ -0,0 +1,11 @@
+package edu.issi.machine.mvc.view;
+
+import javax.swing.JPanel;
+
+class ErrorPanel extends JPanel {
+
+    public void show(String description) {
+	// TODO zaimplementuj
+    }
+
+}
